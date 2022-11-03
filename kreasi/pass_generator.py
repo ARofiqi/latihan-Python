17b6569f1a2e5953ad72f980b60eb08f7c487c8b
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2022-11-03 13:51:24 +0700

    menambah sandi abjad

---
+++ kreasi/pass_generator.py
@@ -0,0 +1,15 @@
+import random
+
+def pasw():
+    jml = int(input("Berapa huruf : "))
+
+    abjad = "aAbBcCdDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
+    simbol = "@#$%^&*()-+|\{>}?<.,-_]`;:"
+    number = "1234567890"
+
+    print("Your pasw : ",end=(""))
+    for i in range(jml):
+        print(random.choice(abjad)+random.choice(simbol)+random.choice(number),end=(""))     
+
+
+pasw()
