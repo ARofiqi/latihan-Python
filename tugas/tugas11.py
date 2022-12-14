9883af2620937f073cc102c329a100318aca8479
Rofiqi2345 <ahmadrofiqi333@gmail.com>
2022-12-14 10:12:35 +0700

    mangupdate tugas 11

--- tugas/tugas11.py
+++ tugas/tugas11.py
@@ -2,13 +2,13 @@
 # Wanita: Berat badan ideal= (tinggi badan-100)-((tinggi badan-100) x 15%)
 # Pria: Berat badan ideal= (tinggi badan-100)-((tinggi badan-100) x 10%)
 
-def bb_normal(tinggi_badan):
+def bb_normal(tinggi_badan=0):
     return (tinggi_badan - 100)
 
-def bb_p_ideal(tinggi_badan):
+def bb_p_ideal(tinggi_badan=0):
     return ((tinggi_badan - 100)-((tinggi_badan - 100)*(15/100)))
 
-def bb_l_ideal(tinggi_badan):
+def bb_l_ideal(tinggi_badan=0):
     return ((tinggi_badan - 100)-((tinggi_badan - 100)*(10/100)))
 
 while True:
@@ -20,17 +20,17 @@ Aplikasi penghitung berat badan
 [4] Keluar""")
     opt = input("Pilih : ")
     if opt == '1':
-        tb = input("\nMasukan Tinggi Badan (cm): ")
+        tb = int(input("\nMasukan Tinggi Badan (cm): "))
         hasil = bb_normal(tb)
-        print(f"Berat badan normal kamu ialah {hasil}")
+        print(f"Berat badan normal kamu ialah {hasil} kg")
     elif opt == '2':
-        tb = input("\nMasukan Tinggi Badan (cm): ")
+        tb = int(input("\nMasukan Tinggi Badan (cm): "))
         hasil = bb_l_ideal(tb)
-        print(f"Berat badan ideal kamu ialah {hasil}")
+        print(f"Berat badan ideal kamu ialah {hasil} kg")
     elif opt == '3':
-        tb = input("\nMasukan Tinggi Badan (cm): ")
+        tb = int(input("\nMasukan Tinggi Badan (cm): "))
         hasil = bb_p_ideal(tb)
-        print(f"Berat badan ideal kamu ialah {hasil}")
+        print(f"Berat badan ideal kamu ialah {hasil} kg")
     elif opt == '4':
         quit = input("\nApakah Kamu Yakin (y/n): ")
         if quit == 'y':break
